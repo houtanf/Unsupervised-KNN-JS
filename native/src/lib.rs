6726c4948fedd2e42f5280ee4f653329b832496c
rex100h <houtanfaridi@gmail.com>
2019-09-14 20:03:41 -0500

    removed comments

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -52,50 +52,6 @@ fn convert_target(target: Vec<Handle<JsValue>>) -> Vec<f64> {
 }
 
 
-//fn convert_neighbors(cx: FunctionContext, neighbors: Vec<Handle<JsValue>>) -> Vec<(String, Vec<f64>)> {
-//    neighbors.iter()
-//             .map(|val| val.downcast::<JsObject>().unwrap() )
-//             .map(|val| convert_object(cx, val) )
-//             .collect()
-//}
-
-
-//fn convert_object(mut cx: FunctionContext, obj: Handle<JsObject>) -> (String, Vec<f64>) {
-//    let label = obj.get(&mut cx, "label").unwrap()
-//                   .downcast::<JsString>().unwrap()
-//                   .value();
-//    let vector = obj.get(&mut cx, "vector").unwrap()
-//                    .downcast::<JsArray>().unwrap()
-//                    .to_vec(&mut cx).unwrap();
-//    (label, convert_target(vector))
-//}
-
-
-//fn to_jsarray(cx: FunctionContext, results: Vec<(String, f64)>) -> Handle<JsArray> {
-//    let objects = results.into_iter()
-//                         .map(|tup| to_jsobject(cx, tup).upcast())
-//                         .collect();
-//    make_jsarray(cx, objects)
-//}
-//
-//fn make_jsarray(mut cx: FunctionContext, objects: Vec<Handle<JsValue>>) -> Handle<JsArray> {
-//    let array = JsArray::new(&mut cx, objects.len() as u32);
-//    for (i, obj) in objects.into_iter().enumerate() {
-//        array.set(&mut cx, i as u32, obj).unwrap();
-//    }
-//    array
-//}
-
-//fn to_jsobject(&mut cx: FunctionContext, (label, dist): (String, f64)) -> Handle<JsObject> {
-//    let obj = JsObject::new(&mut cx);
-//    let js_label = cx.string(label);
-//    let js_dist = cx.number(dist);
-//    obj.set(&mut cx, "label", js_label).unwrap();
-//    obj.set(&mut cx, "distance", js_dist).unwrap();
-//    obj
-//}
-
-
 register_module!(mut cx, {
     cx.export_function("euclideanKnn", euclidean_knn)
 });
