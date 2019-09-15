2c58ce13d0874a04f93043b1bd403b09f7ab022a
rex100h <houtanfaridi@gmail.com>
2019-09-15 18:18:55 -0500

    refactore tuple matching

--- native/src/knn_utils/distance_computation.rs
+++ native/src/knn_utils/distance_computation.rs
@@ -2,6 +2,7 @@ use num::Float;
 
 
 pub fn compute_distance<F: Float>
-  (algo: impl Fn(&Vec<F>, &Vec<F>) -> F, target: &Vec<F>, neighbor: &(String, Vec<F>)) -> (String, F) {
-  (neighbor.0.clone(), algo(target, &neighbor.1))
+  (algo: impl Fn(&Vec<F>, &Vec<F>) -> F, target: &Vec<F>, (label, vector): &(String, Vec<F>)) 
+  -> (String, F) {
+    (label.clone(), algo(target, &vector))
 }
