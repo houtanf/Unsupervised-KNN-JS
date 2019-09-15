c667c9915746f3407fdefd71405898a1677918b4
rex100h <houtanfaridi@gmail.com>
2019-09-15 13:46:38 -0500

    added string to function algorithm lookup

--- native/src/distances/distance_algos.rs
+++ native/src/distances/distance_algos.rs
@@ -1,6 +1,14 @@
 
 
-pub fn euclidean(target: &Vec<f64>, neighbor: &Vec<f64>) -> f64 {
+pub fn get_algo(name: String) -> Result<fn(&Vec<f64>, &Vec<f64>) -> f64, String> {
+  match name.to_lowercase().as_ref() {
+    "euclidean" => Ok( euclidean ),
+    _ => Err( format!("Algorithm {} not found\n", name) ),
+  }
+}
+
+
+fn euclidean(target: &Vec<f64>, neighbor: &Vec<f64>) -> f64 {
   target.iter()
         .zip(neighbor)
         .map(|(t, n)| (t - n).powf(2.0) )
