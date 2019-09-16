ca4ae6f8a72c6fc8c29d7a4df228bd8e2f483725
rex100h <houtanfaridi@gmail.com>
2019-09-15 22:38:44 -0500

    fixed tabbing

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -6,8 +6,8 @@ extern crate rayon;
 use neon::prelude::*;
 
 mod conversions;
-    use conversions::to_rust::{convert_target, convert_neighbors};
-    use conversions::to_js::to_jsarray;
+ use conversions::to_rust::{convert_target, convert_neighbors};
+ use conversions::to_js::to_jsarray;
 mod knn_utils;
  use knn_utils::knn::knn;
 mod distances;
@@ -15,27 +15,27 @@ mod distances;
 
 
 fn default_knn(mut cx: FunctionContext) -> JsResult<JsArray> {
-    let name: String = cx.argument::<JsString>(3)?.value();
-    let algo = get_algo(name).unwrap();
-    run_knn(cx, algo)
+  let name: String = cx.argument::<JsString>(3)?.value();
+  let algo = get_algo(name).unwrap();
+  run_knn(cx, algo)
 }
 
 
 fn run_knn(mut cx: FunctionContext, algo: fn(&Vec<f64>, &Vec<f64>) -> f64) -> JsResult<JsArray> {
-    let k: f64 = cx.argument::<JsNumber>(0)?.value();
-    let neighbors_js = cx.argument::<JsArray>(1)?.to_vec(&mut cx)?;
-    let target_js = cx.argument::<JsArray>(2)?.to_vec(&mut cx)?;
+  let k: f64 = cx.argument::<JsNumber>(0)?.value();
+  let neighbors_js = cx.argument::<JsArray>(1)?.to_vec(&mut cx)?;
+  let target_js = cx.argument::<JsArray>(2)?.to_vec(&mut cx)?;
 
-    let target = convert_target(target_js);
-    let neighbors = convert_neighbors(&mut cx, neighbors_js);
+  let target = convert_target(target_js);
+  let neighbors = convert_neighbors(&mut cx, neighbors_js);
 
 
-    let nearest_neighbors = knn(algo, &target, k, &neighbors).unwrap();
+  let nearest_neighbors = knn(algo, &target, k, &neighbors).unwrap();
 
-    Ok( to_jsarray(cx, nearest_neighbors) )
+  Ok( to_jsarray(cx, nearest_neighbors) )
 }
 
 
 register_module!(mut cx, {
-    cx.export_function("defaultKnn", default_knn)
+  cx.export_function("defaultKnn", default_knn)
 });
