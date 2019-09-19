5068a68afaea4bf61a7e7e0e0caade760f59ced1
rex100h <houtanfaridi@gmail.com>
2019-09-19 01:33:03 -0500

    added cosine distance tests

--- tests/knn.test.js
+++ tests/knn.test.js
@@ -1,33 +1,32 @@
+const { toMatchCloseTo } = require('jest-matcher-deep-close-to')
+expect.extend({ toMatchCloseTo })
+
 const { knn } = require('../lib/knn')
 
 
-knnAlgos = ['euclidean']
+knnAlgos = ['euclidean', 'cosine']
 
 knnTestCases = [
   [1, [{label: 'name', vector: [1, 2]}], [1, 2], [{label: 'name', distance: 0}]],
   [2, [{label: 'name', vector: [1, 2]}], [1, 2], [{label: 'name', distance: 0}]],
   [0, [{label: 'name', vector: [1, 2]}], [1, 2], []],
-  [1, [{label: 'name', vector: [1, 2]}], [1, 2, 3, 4, 5], [{label: 'name', distance: 0}]],
-  [1, [{label: 'name', vector: [1, 2, 3, 4, 5]}], [1, 2], [{label: 'name', distance: 0}]],
-  [1, [{label: 'name', vector: [1, 2]}], [2], [{label: 'name', distance: 1}]],
-  [1, [{label: 'name', vector: [2]}], [1, 2], [{label: 'name', distance: 1}]],
-  [2, [{label: 'name', vector: [1, 2]}, {label: 'other', vector: [3, 2]}], [1, 2],
-     [{label: 'name', distance: 0}, {label: 'other', distance: 2}]],
+  [2, [{label: 'name', vector: [1, 2]}, {label: 'other', vector: [1, 2]}], [1, 2],
+     [{label: 'name', distance: 0}, {label: 'other', distance: 0}]],
   [1, [{label: 'name', vector: [1, 2]}, {label: 'other', vector: [3, 2]}], [1, 2],
      [{label: 'name', distance: 0}]],
-  [2, [{label: 'name', vector: [1, 4]}, {label: 'other', vector: [2, 2]}], [1, 2],
-     [{label: 'other', distance: 1}, {label: 'name', distance: 2}]],
+  [1, [{label: 'name', vector: [1, 4]}, {label: 'other', vector: [1, 2]}], [1, 2],
+     [{label: 'other', distance: 0}]],
 ]
 
 knnTests = knnAlgos.flatMap(algo => 
    knnTestCases.map( test => [algo, ...test])
 )
 
-test.each(knnTests)('Test knn on correct data with %s algo', 
+test.each(knnTests)('Test knn on correct data with %s algo, %d %o %o %o input', 
   function(algo, k, neighbors, target, expected) {
     const output = knn(algo, k, neighbors, target)
 
-    expect(output).toEqual(expected)
+    expect(output).toMatchCloseTo(expected, 5)
   }
  )
 
